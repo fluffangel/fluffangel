1ae5242e41a90b33129da020d2d02a4404d8c107
fluffangel <uwu@fluff.love>
2016-09-06 20:50:35 -0400

    Create instagram_feed.php

---
+++ instagram_feed.php
@@ -0,0 +1,65 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        .instagram_container {
+            position: relative;
+            height: 730px;
+            width: 600px;
+            border: 2px solid #008000;
+            border-radius: 5px;
+            margin: 10px 0 10px 10px;
+            overflow: none;
+            display: inline-block;
+        }
+                .instagram_header, .instagram_footer {
+            background-color: #fff;
+            font-size: 18px;
+            font-weight: 300;
+            color: #292F33;
+            font-family: Helvetica, Roboto, 'Segoe UI', Calibri, sans-serif;
+        }
+        .instagram_header {
+            height: 40px;
+            border-bottom: 1px solid rgba(15,70,100,.12);
+        }
+        .instagram_footer {
+            height: 30px;
+            font-size: 15px;
+            border-top: 1px solid rgba(15,70,100,.12);
+        }
+        .instagram_header a:visited {
+            color: #3b94d9 !important;
+            text-decoration: none !important;
+            outline: 0 !important;
+        }
+        .instagram_header a {
+            color: #3b94d9 !important;
+            text-decoration: none !important;
+            outline: 0 !important;
+        }
+        .instagram_content {
+            overflow-y: scroll;
+            overflow-x: hidden;
+            -webkit-overflow-scrolling: touch;
+            width: 580px;
+            height: 630px;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+            <div class="instagram_container">
+            <div class="instagram_header">
+                <div style="padding: 10px;"><a href="https://www.instagram.com/fluffangel/" target="_blank">@FluffAngel</a>'s Instagram Timeline</div>
+            </div>
+            <div class="instagram_content">
+                <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-version="7" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><a href="https://www.instagram.com/p/BKCHKEXhkWh/"></a></blockquote><blockquote class="instagram-media" data-instgrm-captioned data-instgrm-version="7" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><a href="https://www.instagram.com/p/BKB9iTGBwRF/"></a></blockquote><blockquote class="instagram-media" data-instgrm-captioned data-instgrm-version="7" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><a href="https://www.instagram.com/p/BKBp4IkBx-e/"></a></blockquote><blockquote class="instagram-media" data-instgrm-captioned data-instgrm-version="7" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><a href="https://www.instagram.com/p/BKBFR2PBH5e/"></a></blockquote><blockquote class="instagram-media" data-instgrm-captioned data-instgrm-version="7" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><a href="https://www.instagram.com/p/BKABKDehPw-/"></a></blockquote><blockquote class="instagram-media" data-instgrm-captioned data-instgrm-version="7" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><a href="https://www.instagram.com/p/BJ_zUREhmpT/"></a></blockquote><blockquote class="instagram-media" data-instgrm-captioned data-instgrm-version="7" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><a href="https://www.instagram.com/p/BJ_n3WZBsqS/"></a></blockquote><blockquote class="instagram-media" data-instgrm-captioned data-instgrm-version="7" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"><a href="https://www.instagram.com/p/BJ_kVH6h1vT/"></a></blockquote>            </div>
+            <div class="instagram_footer">
+                <div style="padding: 10px;">Leave nice comments!</div>
+            </div>
+        </div>
+        <script async defer src="//platform.instagram.com/en_US/embeds.js"></script>
+    </body>
+</html>
